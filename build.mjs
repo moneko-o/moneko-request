5dc04c43bb52a9cf681c4e4c12db1a775c9acb80
Moneko <meow.moneko@gmail.com>
2026-04-01 16:52:49 +0800

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -1,25 +1,45 @@
 import { convert } from '@moneko/convert';
 import { ESLint } from '@moneko/eslint';
 
-console.log('lint start');
-console.time('lint end');
+console.log('Lint runing...');
+console.time('Lint');
 const eslint = new ESLint({
   fix: true,
 });
-const [results, formatter] = await Promise.all([eslint.lintFiles('src'), eslint.loadFormatter('stylish')]);
-const [resultText] = await Promise.all([formatter.format(results), ESLint.outputFixes(results)]);
 
-process.stdout.write(resultText);
-console.timeEnd('lint end');
+try {
+  const [results, formatter] = await Promise.all([
+    eslint.lintFiles('src'),
+    eslint.loadFormatter('stylish'),
+  ]);
+
+  const resultText = await formatter.format(results);
+
+  process.stdout.write(resultText);
+  await ESLint.outputFixes(results);
+
+  console.timeEnd('Lint');
+} catch (err) {
+  throw err;
+}
+
 const common = {
   jsc: {
     parser: {
       syntax: 'typescript',
       decorators: true,
       dynamicImport: true,
+      isolatedModules: true,
     },
     target: 'esnext',
     loose: true,
+    minify: {
+      mangle: true,
+      compress: true,
+      format: {
+        comments: 'some',
+      },
+    },
     experimental: {
       emitIsolatedDts: true,
     },
@@ -27,10 +47,22 @@ const common = {
   minify: true,
 };
 
-Promise.all([
+await Promise.all([
+  convert({
+    outDir: 'esm',
+    inputDir: 'src',
+    ignore: [/__tests__/],
+    options: {
+      ...common,
+      module: {
+        type: 'es6',
+      },
+    },
+  }),
   convert({
     outDir: 'cjs',
     inputDir: 'src',
+    ignore: [/__tests__/],
     options: {
       ...common,
       module: {
@@ -38,14 +70,8 @@ Promise.all([
       },
     },
   }),
-  convert({
-    outDir: 'esm',
-    inputDir: 'src',
-    options: {
-      ...common,
-      module: {
-        type: 'es6',
-      },
-    },
-  })
-])
+]).then((childs) => {
+  childs.forEach((result) => {
+    result.failed.map((msg) => process.stdout.write(msg));
+  });
+});
